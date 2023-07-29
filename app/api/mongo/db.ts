3f6c3635e6a1c5382df6b40d3bdf73d0c5822e3c
ssolson <ssolson@sandia.gov>
2023-07-29 15:21:37 -0600

    types on db

--- app/api/mongo/db.ts
+++ app/api/mongo/db.ts
@@ -1,5 +1,19 @@
 // mongo/mongodb.js
-import { MongoClient } from "mongodb";
+import { MongoClient, Db } from "mongodb";
+
+interface Client {
+  promise?: Promise<{ client: MongoClient; db: Db }>;
+  conn?: any; // Replace 'any' with the actual type of 'conn' if known.
+}
+
+interface MongoInstance {
+  conn?: MongoClient;
+  promise?: Promise<{ client: MongoClient; db: Db }>;
+}
+
+declare global {
+  var mongo: MongoInstance;
+}
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -8,7 +22,7 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client: Client;
 
 if (!process.env.ATLAS_URI) {
   throw new Error("Please define the ATLAS_URI environment variable");
@@ -18,7 +32,7 @@ if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable to retain the MongoClient instance
   // across hot-reloads to avoid multiple MongoClient instances.
   if (!global.mongo) {
-    global.mongo = { conn: null, promise: null };
+    global.mongo = { conn: undefined, promise: undefined };
   }
   client = global.mongo;
 } else {
@@ -34,8 +48,12 @@ if (!client.promise) {
 }
 
 const getClientAndDb = async () => {
-  const { client: clientInstance, db } = await client.promise;
-  return { client: clientInstance, db };
+  if (client.promise) {
+    const { client: clientInstance, db } = await client.promise;
+    return { client: clientInstance, db };
+  } else {
+    throw new Error("Client promise is not defined");
+  }
 };
 
 export { getClientAndDb };
