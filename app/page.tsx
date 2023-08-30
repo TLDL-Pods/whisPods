d95ee95d46419ad1f9efe9408cb62dba23c0fbc4
ssolson <ssolson@sandia.gov>
2023-08-30 13:09:47 -0600

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 "use client";
 
+import { Analytics } from "@vercel/analytics/react";
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect, useContext } from "react";
-import PageSelect from "./components/PageSelect";
-import { useEpisodeContext } from "./hooks/useEpisodeContext";
 import { EpisodeProps, SegmentProps } from "@/types";
-import { RiMegaphoneLine } from "react-icons/ri";
 import TDG from "./assets/the-daily-gwei.jpg";
 
 export default function Home() {
   const [latestEpisode, setLatestEpisode] = useState<EpisodeProps>();
-  const { data, setData } = useEpisodeContext();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [episodesPerPage] = useState(10);
-  const indexOfLastEpisode = currentPage * episodesPerPage;
-  const indexOfFirstEpisode = indexOfLastEpisode - episodesPerPage;
-  const [startIndex, setStartIndex] = useState(0);
-  const [animationState, setAnimationState] = useState<
-    "slide-in" | "slide-out"
-  >("slide-in");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,29 +21,6 @@ export default function Home() {
     fetchData();
     console.log("data", latestEpisode);
   }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (latestEpisode && animationState === "slide-out") {
-        setStartIndex(
-          (prevIndex) => (prevIndex + 4) % latestEpisode.episode_data.length
-        );
-        setAnimationState("slide-in");
-      } else if (animationState === "slide-in") {
-        setAnimationState("slide-out");
-      }
-    }, 6000);
-
-    return () => clearInterval(interval);
-  }, [latestEpisode, animationState]);
-
-  const displayedSegments = latestEpisode?.episode_data
-    .sort((a: SegmentProps, b: SegmentProps) => {
-      const lengthA = a.end_time_ms - a.start_time_ms;
-      const lengthB = b.end_time_ms - b.start_time_ms;
-      return lengthB - lengthA;
-    })
-    .slice(startIndex, startIndex + 4);
 
   return (
     <div className="flex-col justify-center w-full p-4">
@@ -97,72 +63,8 @@ export default function Home() {
             </div>
           </div>
         </Link>
-
-        {/* <div className="w-full max-w-2xl mx-auto">
-          <Link
-            href={`/podcasts/thedailygwei/episode/${latestEpisode?.episode_number}`}
-          >
-            <div className="relative w-full overflow-x-hidden border-b hero h-1/2 border-opacity-40">
-              {latestEpisode && (
-                <div className="absolute inset-0">
-                  <img
-                    src="https://picsum.photos/1920/1080/"
-                    alt="Background"
-                    className="object-cover w-full h-full"
-                  />
-                </div>
-              )}
-              <div className="relative flex justify-center w-full h-full">
-                <div className="flex h-full w-full items-center justify-start bg-gradient-to-r from-stone-950 via-stone-950 via-65% to-transparent">
-                  <div className="w-2/3 h-full p-12">
-                    <h1 className="text-xl text-center text-violet-200">
-                      LATEST EPISODE
-                    </h1>
-                    <p className="mb-5 text-center">
-                      {latestEpisode?.release_date}
-                    </p>
-                    <h2 className="text-2xl text-center text-violet-400">
-                      TDGR #{latestEpisode?.episode_number}
-                    </h2>
-                    <h1 className="text-4xl text-center">
-                      {latestEpisode?.episode_title_generated?.toUpperCase()}
-                    </h1>
-                    <div className="flex flex-col h-40 p-6 mx-auto my-auto transition-all duration-500 animatedHeadlines w-fit">
-                      {displayedSegments?.map(
-                        (segment: SegmentProps, index: number) => (
-                          <div
-                            key={segment.segment_number}
-                            className={animationState}
-                            onAnimationEnd={(e) => {
-                              if (e.animationName === "slideInFromLeft") {
-                                setAnimationState("slide-out");
-                              }
-                            }}
-                          >
-                            {segment.headline.length > 1 && (
-                              <div className="flex mx-auto">
-                                <div className="my-auto">
-                                  <RiMegaphoneLine />
-                                </div>
-                                <p className="ml-2">{segment.headline}</p>
-                              </div>
-                            )}
-                          </div>
-                        )
-                      )}
-                    </div>
-                    <div className="flex w-full">
-                      <button className="px-4 py-2 mx-auto transition-all duration-500 bg-opacity-50 rounded-lg btn bg-violet-700 hover:bg-opacity-100">
-                        View More
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </div> */}
       </div>
+      <Analytics />
     </div>
   );
 }
