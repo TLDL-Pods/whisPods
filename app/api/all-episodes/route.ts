b14334138c9b21d4553fe6bcbe59796949f8ac2c
ssolson <ssolson@sandia.gov>
2023-09-22 14:36:17 -0600

    revalidate every 5 min

--- app/api/all-episodes/route.ts
+++ app/api/all-episodes/route.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from "next/server";
 import { getClientAndDb } from "../mongo/db";
 
-export const revalidate = 3600;
+export const revalidate = 300;
 export async function GET() {
   try {
     const { db } = await getClientAndDb();
