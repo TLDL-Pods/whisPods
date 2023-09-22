9a5cb299d2b65ef974b117643f6c016877ac2ea9
ssolson <ssolson@sandia.gov>
2023-09-22 15:51:16 -0600

    reduce space between image and content

--- app/components/PageSelect.tsx
+++ app/components/PageSelect.tsx
@@ -44,68 +44,70 @@ export default function PageSelect({ episode }: PageSelectProps) {
           </div>
         </div>
 
-        {/* Image container for larger screens */}
-        <div className="items-center justify-center w-1/2 h-full md:flex">
-          <div className="relative hidden w-64 h-64 mr-10 md:block">
-            <Image
-              src={TDG}
-              alt="Background"
-              className="object-cover w-full h-full"
-            />
-            {/* Number overlay */}
-            <div
-              className="absolute transform -translate-x-1/2 top-1/2 left-1/2 -translate-y-3/5"
-              style={{ transform: "translate(-60%, -150%) rotate(-32deg)" }}
-            >
-              <span className="text-xl font-bold text-black">
-                {episode.episode_number}
-              </span>
-            </div>
-            {/* Date overlay */}
-            <div className="absolute bottom-0 pb-1 transform -translate-x-1/2 left-1/2">
-              <span className="text-xl font-bold text-black">
-                {episode.release_date}
-              </span>
+        <div className="flex items-center mx-auto md:max-w-5xl">
+          {/* Image container for larger screens */}
+          <div className="hidden md:flex md:w-1/3">
+            <div className="relative hidden w-64 h-64 mr-10 md:block">
+              <Image
+                src={TDG}
+                alt="Background"
+                className="object-cover w-full h-full"
+              />
+              {/* Number overlay */}
+              <div
+                className="absolute transform -translate-x-1/2 top-1/2 left-1/2 -translate-y-3/5"
+                style={{ transform: "translate(-60%, -150%) rotate(-32deg)" }}
+              >
+                <span className="text-xl font-bold text-black">
+                  {episode.episode_number}
+                </span>
+              </div>
+              {/* Date overlay */}
+              <div className="absolute bottom-0 pb-1 transform -translate-x-1/2 left-1/2">
+                <span className="text-xl font-bold text-black">
+                  {episode.release_date}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-col justify-center w-1/2 md:ml-1">
-          <div className="hidden w-full mb-4 md:block">
-            <div className="text-l md:text-2xl text-violet-100">
-              {episode.episode_title
-                ? episode.episode_title.toUpperCase()
+          <div className="flex flex-col justify-center w-full md:w-2/3">
+            <div className="hidden w-full mb-4 md:block">
+              <div className="text-l md:text-2xl text-violet-100">
+                {episode.episode_title
+                  ? episode.episode_title.toUpperCase()
+                  : null}
+              </div>
+            </div>
+
+            {/* Segments */}
+            <div className="hidden pb-4 mx-auto mt-2 md:block w-fit">
+              {topThreeSegments.map((segment, index) => (
+                <div
+                  key={segment.segment_number}
+                  className="flex mx-auto my-auto"
+                >
+                  <div className="mt-1 text-violet-400">
+                    <RiMegaphoneLine />
+                  </div>
+
+                  <p className="ml-2 text-left">{segment.headline}</p>
+                </div>
+              ))}
+            </div>
+            {/* Keywords */}
+            <div className="hidden mx-auto text-xs md:flex">
+              {episode.episode_keywords && episode.episode_keywords.length > 0
+                ? episode.episode_keywords.map((keyword, index) => (
+                    <div
+                      key={index}
+                      className="p-1 px-2 mx-2 my-auto rounded-lg bg-violet-600 bg-opacity-30"
+                    >
+                      <p>{keyword.toUpperCase()}</p>
+                    </div>
+                  ))
                 : null}
             </div>
-          </div>
-
-          {/* Segments */}
-          <div className="hidden pb-4 mx-auto mt-2 md:block w-fit">
-            {topThreeSegments.map((segment, index) => (
-              <div
-                key={segment.segment_number}
-                className="flex mx-auto my-auto"
-              >
-                <div className="mt-1 text-violet-400">
-                  <RiMegaphoneLine />
-                </div>
-
-                <p className="ml-2 text-left">{segment.headline}</p>
-              </div>
-            ))}
-          </div>
-          {/* Keywords */}
-          <div className="hidden mx-auto text-xs md:flex">
-            {episode.episode_keywords && episode.episode_keywords.length > 0
-              ? episode.episode_keywords.map((keyword, index) => (
-                  <div
-                    key={index}
-                    className="p-1 px-2 mx-2 my-auto rounded-lg bg-violet-600 bg-opacity-30"
-                  >
-                    <p>{keyword.toUpperCase()}</p>
-                  </div>
-                ))
-              : null}
           </div>
         </div>
       </div>
