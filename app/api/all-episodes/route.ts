03de818825c56f93d170b6f6724b042d15bff20e
ssolson <ssolson@sandia.gov>
2023-09-22 14:35:06 -0600

    revalidate on hour

--- app/api/all-episodes/route.ts
+++ app/api/all-episodes/route.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from "next/server";
 import { getClientAndDb } from "../mongo/db";
 
-export const revalidate = 1;
+export const revalidate = 3600;
 export async function GET() {
   try {
     const { db } = await getClientAndDb();
