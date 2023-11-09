79466c0d088f7488e8d740e8a48ace5320e66d59
ssolson <ssolson@sandia.gov>
2023-11-09 15:02:17 -0700

    center segments on larger screen

--- app/thedailygwei/[ep_number]/page.tsx
+++ app/thedailygwei/[ep_number]/page.tsx
@@ -1,14 +1,14 @@
-'use client';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useRouter } from "next/navigation";
 
-import { EpisodeProps, SegmentProps } from '@/types';
-import { useState, useEffect, useRef, createRef } from 'react';
-import { IoArrowBack } from 'react-icons/io5';
+import { EpisodeProps, SegmentProps } from "@/types";
+import { useState, useEffect, useRef, createRef } from "react";
+import { IoArrowBack } from "react-icons/io5";
 
-import { ImListNumbered } from 'react-icons/im';
-import { BiSolidTimer, BiXCircle } from 'react-icons/bi';
+import { ImListNumbered } from "react-icons/im";
+import { BiSolidTimer, BiXCircle } from "react-icons/bi";
 
-import Segment from '@/app/components/Segment';
+import Segment from "@/app/components/Segment";
 
 export default function EpisodePage({
   params,
@@ -44,7 +44,7 @@ export default function EpisodePage({
   };
 
   const [currentEpisode, setCurrentEpisode] = useState<number>(
-    parseInt(params.ep_number, 10),
+    parseInt(params.ep_number, 10)
   );
 
   useEffect(() => {
@@ -52,18 +52,18 @@ export default function EpisodePage({
       const episodeNumber = parseInt(params.ep_number, 10);
       const res = await fetch(`/api/episode?episode_number=${episodeNumber}`);
       const json = await res.json();
-      const filteredSegments = json['data'].episode_data.filter(
-        (segment: SegmentProps) => segment.segment_number !== 0,
+      const filteredSegments = json["data"].episode_data.filter(
+        (segment: SegmentProps) => segment.segment_number !== 0
       );
       const sortedData = {
-        ...json['data'],
+        ...json["data"],
         episode_data: filteredSegments.sort(
           (a: SegmentProps, b: SegmentProps) =>
-            b.segment_length_ms - a.segment_length_ms,
+            b.segment_length_ms - a.segment_length_ms
         ),
       };
       segmentRefs.current = sortedData.episode_data.map(() =>
-        createRef<HTMLDivElement>(),
+        createRef<HTMLDivElement>()
       );
       setData(sortedData);
     };
@@ -74,18 +74,18 @@ export default function EpisodePage({
     const fetchData = async () => {
       const res = await fetch(`/api/episode?episode_number=${currentEpisode}`);
       const json = await res.json();
-      const filteredSegments = json['data'].episode_data.filter(
-        (segment: SegmentProps) => segment.segment_number !== 0,
+      const filteredSegments = json["data"].episode_data.filter(
+        (segment: SegmentProps) => segment.segment_number !== 0
       );
       const sortedData = {
-        ...json['data'],
+        ...json["data"],
         episode_data: filteredSegments.sort(
           (a: SegmentProps, b: SegmentProps) =>
-            b.segment_length_ms - a.segment_length_ms,
+            b.segment_length_ms - a.segment_length_ms
         ),
       };
       segmentRefs.current = sortedData.episode_data.map(() =>
-        createRef<HTMLDivElement>(),
+        createRef<HTMLDivElement>()
       );
       setData(sortedData);
       console.log(sortedData);
@@ -98,27 +98,29 @@ export default function EpisodePage({
   }
 
   return (
-    <div className="flex flex-col justify-center h-full min-w-screen relative">
+    <div className="relative flex flex-col justify-center h-full min-w-screen">
+      {/* Segment Header */}
       <div className="w-full max-w-screen">
-        {/* Main Content */}
         <div className="">
-          {/* !!!Replace with podcast title from data */}
-          <div className="text-center text-lg font-semibold">
-            The Daily Gwei
+          <div className="text-lg font-semibold text-center text-violet-400">
+            The Daily Gwei Refuel
+          </div>
+          <div className="text-lg text-center text-violet-400">
+            {data.episode_title}
           </div>
 
           <div className="pt-1">
-            <h3 className="font-semibold text-center text-violet-200">
+            <h3 className="font-semibold text-center text-violet-300">
               Episode {data.episode_number}
             </h3>
-            <h3 className="text-center text-violet-200">{data.release_date}</h3>
+            <h3 className="text-center text-violet-300">{data.release_date}</h3>
           </div>
         </div>
         <div className="">
           <div className="flex justify-between mx-4">
             {/* Back to Episodes button */}
             <button
-              onClick={() => router.push('/')}
+              onClick={() => router.push("/")}
               className="font-bold text-white rounded hover:bg-stone-900"
             >
               <IoArrowBack size={24} className="inline-block" />
@@ -134,20 +136,21 @@ export default function EpisodePage({
           </div>
         </div>
       </div>
-      <div className="mt-4 h-fit w-full">
-        <ul>
+      {/* Segments */}
+      <div className="flex justify-center w-full mt-4">
+        <ul className="list-none">
           {data.episode_data
             .sort(
               isOrganizedByLength
                 ? (a: SegmentProps, b: SegmentProps) =>
                     b.segment_length_ms - a.segment_length_ms
                 : (a: SegmentProps, b: SegmentProps) =>
-                    a.segment_number - b.segment_number,
+                    a.segment_number - b.segment_number
             )
             .map((segment, index) => (
               <div
                 key={segment.segment_number}
-                className="border-y border-violet-100 border-opacity-60 bg-neutral-950"
+                className=" border-y border-violet-100 border-opacity-60 bg-neutral-950"
               >
                 <Segment
                   episodeNumber={data.episode_number}
@@ -163,9 +166,9 @@ export default function EpisodePage({
                   setShowSegmentIndex={setShowSegmentIndex}
                   handleSummaryToggle={handleSummaryToggle}
                   onSegmentClick={(index) => {
-                    history.pushState(null, '', `#segment-${index}`);
+                    history.pushState(null, "", `#segment-${index}`);
                     setSelectedSegmentIndex(
-                      selectedSegmentIndex === index ? null : index,
+                      selectedSegmentIndex === index ? null : index
                     );
                     const segmentRef = segmentRefs.current[index]?.current;
                     if (segmentRef) {
