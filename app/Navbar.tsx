0910d2d3c8d18fedc32bb27c1039ca38bc37cf9d
ssolson <ssolson@sandia.gov>
2023-12-13 16:46:02 -0700

    fix broken header on ep page

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { GiSoundWaves } from 'react-icons/gi';
-import YouTubeEmbed from './components/YoutubeEmbed';
-import YoutubeVideo from './components/YoutubeVideo';
-import { useApp } from './hooks/useApp';
+import Link from "next/link";
+import { GiSoundWaves } from "react-icons/gi";
+import YouTubeEmbed from "./components/YoutubeEmbed";
+import YoutubeVideo from "./components/YoutubeVideo";
+import { useApp } from "./hooks/useApp";
 
 export default function Navbar({ children }: { children: React.ReactNode }) {
   const { state } = useApp();
 
   return (
-    <div className=" bg-stone-950 max-w-screen h-screen relative overflow-x-hidden">
-      <header className="text-xl font-semibold max-w-screen text-gray-100 bg-stone-950  ">
+    <div className=" bg-stone-950 max-w-screen">
+      <header className="text-xl font-semibold text-gray-100 max-w-screen bg-stone-950 ">
         <div className="flex items-center justify-between w-full p-6">
           {/* TLDL Title */}
-          <Link href={'/'}>
+          <Link href={"/"}>
             <div className="flex items-center my-auto text-4xl font-bold cursor-pointer">
               <GiSoundWaves className="my-auto mr-2 text-6xl" />
               <p className="my-auto">TLDL</p>
@@ -33,12 +33,12 @@ export default function Navbar({ children }: { children: React.ReactNode }) {
         </div>
       </header>
 
-      <div className="text-gray-100 h-full w-screen">{children}</div>
+      <div className="w-screen h-full text-gray-100">{children}</div>
       <div
         className={`${
           state.isVideoModalOpen
-            ? 'h-[350px] border-t border-violet-800'
-            : 'h-0'
+            ? "h-[350px] border-t border-violet-800"
+            : "h-0"
         } w-full bottom-0 fixed bg-stone-900  duration-300`}
       >
         <YoutubeVideo />
