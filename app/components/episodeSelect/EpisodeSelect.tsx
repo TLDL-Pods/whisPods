484327adc5e1df57cf174efa0056df1d0abef1e2
Christian <cjwinkler0@gmail.com>
2023-12-29 15:30:55 -0500

    refactor EpisodeSelect

--- app/components/episodeSelect/EpisodeSelect.tsx
+++ app/components/episodeSelect/EpisodeSelect.tsx
@@ -3,26 +3,30 @@ import Image from 'next/image';
 import { EpisodeProps } from '@/types';
 import { RiMegaphoneLine } from 'react-icons/ri';
 import TDG from '@/app/assets/the-daily-gwei.jpg';
-import { useApp } from '@/app/hooks/useApp';
 
 interface EpisodeSelectProps {
   episode: EpisodeProps;
 }
 
-/* REFACTOR LATER!!!! */
-
 export default function EpisodeSelect({ episode }: EpisodeSelectProps) {
   const topThreeSegments = episode.episode_data
     .sort((a, b) => b.segment_length_ms - a.segment_length_ms)
     .slice(0, 3);
-  const { state, setState } = useApp();
 
   return (
     <Link href={`/thedailygwei/${episode.episode_number}`}>
       <div className="flex flex-col flex-grow w-full min-h-full text-center transition-all duration-500 border-t bg-stone-950 border-violet-400 border-opacity-40 hover:bg-stone-800">
         {/* Image, Episode Number, and Date for mobile */}
-        <div className="flex items-center w-full mb-3 lg:w-auto lg:hidden">
-          <div className="relative w-24 h-16 mr-1 lg:w-64 lg:h-64">
+        <div className="flex items-center mx-auto w-full lg:w-1/2 lg:p-0 p-2">
+          <div className="flex flex-col mr-2">
+            <span className=" font-bold text-violet-300 lg:text-2xl lg:border-b border-violet-300 border-opacity-40 lg:pb-1 lg:mb-2">
+              {episode.episode_number}
+            </span>
+            <span className="text-xs text-white text-opacity-80 lg:text-sm">
+              {episode.release_date}
+            </span>
+          </div>
+          <div className="w-24 h-16 mr-1 lg:w-64 lg:h-auto lg:p-4">
             {/* Image container */}
             <Image
               src={TDG}
@@ -31,66 +35,22 @@ export default function EpisodeSelect({ episode }: EpisodeSelectProps) {
             />
           </div>
 
-          <div className="flex flex-col mb-2 mr-2">
-            <span className="text-sm font-bold text-white lg:text-xl lg:text-black">
-              {episode.episode_number}
-            </span>
-            <span className="text-sm font-bold text-white lg:text-xl lg:text-black">
-              {episode.release_date}
-            </span>
-          </div>
-          <div className="w-full mb-2">
-            <div className="text-base lg:text-2xl text-violet-100">
+          <div className="w-full">
+            <div className="lg:text-2xl lg:text-left text-violet-300">
               {episode.episode_title
                 ? episode.episode_title.toUpperCase()
                 : null}
             </div>
-          </div>
-        </div>
-
-        {/* Image, Episode Number, and Date for larger screens */}
-        <div className="flex items-center justify-center">
-          {/* Image */}
-          <div className="relative hidden w-32 lg:block">
-            <Image
-              src={TDG}
-              alt="Background"
-              className="object-cover w-full h-full"
-            />
-          </div>
-
-          {/* Title, Date, Segments, Keywords */}
-          <div className="justify-center w-1/2">
-            <div className="grid grid-cols-12 gap-x-0 lg:text-left">
-              {/* Episode Number*/}
-              <div className="hidden mt-2 mr-1 lg:text-end lg:block text-l lg:text-xl text-violet-100 lg:col-span-1 lg:row-span-3">
-                {episode.episode_number}:{' '}
-              </div>
-              {/* Title*/}
-              <div className="justify-start hidden mt-2 lg:block text-l lg:text-xl text-violet-100 lg:col-span-9">
-                {episode.episode_title
-                  ? episode.episode_title.toUpperCase()
-                  : null}
-              </div>
-              {/* Date */}
-              <div className="hidden mt-2 lg:text-right lg:block text-l lg:text-sm text-violet-100 lg:col-span-2">
-                {episode.release_date}
-              </div>
-              {/* Segments */}
-              <div className="hidden mt-2 mb-2 lg:text-center lg:block text-l lg:text-l text-violet-100 lg:col-span-9">
-                {topThreeSegments.map((segment, index) => (
-                  <div
-                    key={segment.segment_number}
-                    className="flex mx-auto my-auto"
-                  >
-                    <div className="mt-1 text-violet-400">
-                      <RiMegaphoneLine />
-                    </div>
-
-                    <p className="ml-2 text-left">{segment.segment_title}</p>
+            <div className="mt-2 text-violet-100 hidden lg:block">
+              {topThreeSegments.map((segment, index) => (
+                <div key={segment.segment_number} className="flex">
+                  <div className="mt-1 text-violet-400">
+                    <RiMegaphoneLine />
                   </div>
-                ))}
-              </div>
+
+                  <p className="ml-2 text-left">{segment.segment_title}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
