3064fadf40b86512dd52e38584033060d616496a
ssolson <sterlingolson@yahoo.com>
2024-02-07 19:09:53 -0700

    example frame

--- app/frame/page.tsx
+++ app/frame/page.tsx
@@ -1,19 +1,128 @@
-export default function Page() {
-  const title = 'Onframe Chess';
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameInput,
+  FrameReducer,
+  NextServerPageProps,
+  getPreviousFrame,
+  useFramesReducer,
+  getFrameMessage,
+} from 'frames.js/next/server';
+import Link from 'next/link';
+import { HubHttpUrlOptions } from 'frames.js';
+import { getTokenUrl } from 'frames.js';
 
+export const DEBUG_HUB_OPTIONS: HubHttpUrlOptions = {
+  hubHttpUrl:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/debug/hub'
+      : undefined,
+};
+
+type State = {
+  active: string;
+  total_button_presses: number;
+};
+
+const initialState = { active: '1', total_button_presses: 0 };
+
+const reducer: FrameReducer<State> = (state, action) => {
+  return {
+    total_button_presses: state.total_button_presses + 1,
+    active: action.postBody?.untrustedData.buttonIndex
+      ? String(action.postBody?.untrustedData.buttonIndex)
+      : '1',
+  };
+};
+
+// This is a react server component only
+export default async function Home({
+  params,
+  searchParams,
+}: NextServerPageProps) {
+  const previousFrame = getPreviousFrame<State>(searchParams);
+
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    ...DEBUG_HUB_OPTIONS,
+    fetchHubContext: true,
+  });
+
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error('Invalid frame payload');
+  }
+
+  const [state, dispatch] = useFramesReducer<State>(
+    reducer,
+    initialState,
+    previousFrame
+  );
+
+  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
+  // example: load the users credentials & check they have an NFT
+
+  // Example with satori and sharp:
+  // const imageUrl = await
+  frameMessage;
+
+  console.log('info: state is:', state);
+
+  if (frameMessage) {
+    const {
+      isValid,
+      buttonIndex,
+      inputText,
+      castId,
+      requesterFid,
+      casterFollowsRequester,
+      requesterFollowsCaster,
+      likedCast,
+      recastedCast,
+      requesterVerifiedAddresses,
+      requesterUserData,
+    } = frameMessage;
+
+    console.log('info: frameMessage is:', frameMessage);
+  }
+
+  const baseUrl = process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000';
+
+  // then, when done, return next frame
   return (
-    <html>
-      <head>
-        <title>Onframe Chess</title>
-        <meta property="og:title" content="Hello World" />
-        <meta property="og:image" content="Some image" />
-        <meta name="fc:frame" content="vNext" />
-        <meta name="fc:frame:image" content="Just another image" />
-        <meta name="fc:frame:post_url" content="eeeeeeee" />
-        <meta name="fc:frame:button:1" content="Hey!" />
-        <meta name="fc:frame:button:2" content="What Up?" />
-        <meta name="fc:frame:button:3" content="Hello!" />
-      </head>
-    </html>
+    <div className="p-4">
+      TLDL.
+      <Link href={`/debug?url=${baseUrl}`} className="underline">
+        Debug
+      </Link>
+      <FrameContainer
+        postUrl="/frames"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        {/* <FrameImage src="https://framesjs.org/og.png" /> */}
+        <FrameImage>
+          <div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
+            {frameMessage?.inputText ? frameMessage.inputText : 'Hello world'}
+          </div>
+        </FrameImage>
+        <FrameInput text="put some text here" />
+        <FrameButton onClick={dispatch}>
+          {state?.active === '1' ? 'Active' : 'Inactive'}
+        </FrameButton>
+        <FrameButton onClick={dispatch}>
+          {state?.active === '2' ? 'Active' : 'Inactive'}
+        </FrameButton>
+        <FrameButton
+          mint={getTokenUrl({
+            address: '0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df',
+            tokenId: '123',
+            chainId: 7777777,
+          })}
+        >
+          Mint
+        </FrameButton>
+        <FrameButton href={`https://www.google.com`}>External</FrameButton>
+      </FrameContainer>
+    </div>
   );
 }
