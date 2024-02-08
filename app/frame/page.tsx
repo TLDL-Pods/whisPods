9813323062e8008bbe7bcff9f0212e7e4f91d771
ssolson <sterlingolson@yahoo.com>
2024-02-07 18:29:26 -0700

    initial frame

---
+++ app/frame/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  const title = 'Onframe Chess';
+
+  return (
+    <html>
+      <head>
+        <title>Onframe Chess</title>
+        <meta property="og:title" content="Hello World" />
+        <meta property="og:image" content="Some image" />
+        <meta name="fc:frame" content="vNext" />
+        <meta name="fc:frame:image" content="Just another image" />
+        <meta name="fc:frame:post_url" content="eeeeeeee" />
+        <meta name="fc:frame:button:1" content="Hey!" />
+        <meta name="fc:frame:button:2" content="What Up?" />
+        <meta name="fc:frame:button:3" content="Hello!" />
+      </head>
+    </html>
+  );
+}
