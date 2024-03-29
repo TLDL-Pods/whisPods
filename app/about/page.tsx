948e02f9e537ca0551f824469ee6f4273966f945
Christian <cjwinkler0@gmail.com>
2024-03-29 16:37:24 -0400

    about style overhaul/ add data

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,60 +1,53 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import sters from "@/app/assets/sters.gif";
-import birdBones from "@/app/assets/birdbones.gif";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { teamMembers, aboutQuestions } from '../data/aboutData';
 
 export default function About() {
-  const teamMembers = [
-    {
-      id: 1,
-      name: "Sters",
-      position: "Developer",
-      bio: "All he knows is that he is building this page and he is not sure why. Sters Bot is a G4-Y Model assault bot that has been modified for mathematics specialization and Ethereum retrieval. The empty husk, powered only by profits and decentralization, moves forward with robotic precision.",
-      image: sters,
-      twitter: "https://twitter.com/0xSters",
-      twitterHandle: "@0xSters",
-    },
-    {
-      id: 2,
-      name: "Birdbonez",
-      position: "Frontend",
-      bio: "Everyone needs a guy. I got a guy. Birdbones was born with hollow, super-fast bones giving him the ability to shit on anyone, instantly, for no reason, at any given time. Using the power of his kindergarten diploma and powerful Truck Nuts For Hats amulet he condescends even the most accomplished warriors. ",
-      image: birdBones,
-    },
-  ];
-
   return (
-    <div className="flex justify-center w-full p-4">
-      <div className="w-full max-w-2xl">
-        <h1 className="mb-6 text-2xl font-bold"> About</h1>
-        MP3s to Bullet points. Right now we support The Daily Gwei.
-        <h1 className="mb-6 text-2xl font-bold"> Team</h1>
-        <div className="flex flex-col space-y-6">
+    <div className="flex justify-center p-6 pt-12 lg:pt-20">
+      <div className="w-full max-w-5xl ">
+        <h1 className="text-center text-4xl font-semibold text-secondary">
+          ABOUT
+        </h1>
+
+        <div className="mx-auto mt-4 flex max-w-4xl flex-col gap-8 lg:mt-12">
+          {aboutQuestions.map((question, i) => (
+            <div key={i} className="mt-4">
+              <h3 className="text-3xl font-semibold">{question.q}</h3>
+              <p className="mt-2">{question.a}</p>
+            </div>
+          ))}
+        </div>
+        <div className="mx-auto my-12 w-4/5 border-b border-baseText1 lg:my-24"></div>
+        <h1 className="text-center text-4xl font-semibold text-secondary">
+          THE TEAM
+        </h1>
+        <div className="mt-12 flex flex-col gap-2 space-y-6 lg:mt-6 lg:flex-row lg:space-y-0">
           {teamMembers.map((member) => (
-            <div key={member.id} className="p-4 border rounded">
+            <div
+              key={member.id}
+              className="flex flex-col rounded-lg lg:h-[600px] lg:p-12"
+            >
               <Image
                 src={member.image}
                 alt={member.name}
-                className="w-64 h-64 mx-auto "
-                width={100}
-                height={100}
+                className="mx-auto h-64 w-64 rounded-full border-4 border-baseText lg:h-72 lg:w-72"
               />
-              <h2 className="mt-4 text-xl font-semibold text-center">
+              <h2 className="mt-4 text-center text-2xl font-semibold">
                 {member.name}
               </h2>
-              {member.twitterHandle && (
-                <p className="mt-2 text-center">
-                  <Link
-                    href={member.twitter}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {member.twitterHandle}
-                  </Link>
-                </p>
-              )}
-              <p className="text-center text-gray-500">{member.position}</p>
+
+              <p className="h-6 text-center">
+                <Link
+                  href={member.twitter ? member.twitter : ''}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {member.twitterHandle ? member.twitterHandle : ''}
+                </Link>
+              </p>
+
               <p className="mt-2">{member.bio}</p>
             </div>
           ))}
---
+++ app/data/aboutData.tsx
@@ -0,0 +1,36 @@
+import sters from '@/app/assets/sters.gif';
+import birdBones from '@/app/assets/birdbones.gif';
+
+export const teamMembers = [
+  {
+    id: 1,
+    name: 'Sters',
+    position: 'Developer',
+    bio: 'All he knows is that he is building this page and he is not sure why. Sters Bot is a G4-Y Model assault bot that has been modified for mathematics specialization and Ethereum retrieval. The empty husk, powered only by profits and decentralization, moves forward with robotic precision.',
+    image: sters,
+    twitter: 'https://twitter.com/0xSters',
+    twitterHandle: '@0xSters',
+  },
+  {
+    id: 2,
+    name: 'Birdbonez',
+    position: 'Developer',
+    bio: 'Everyone needs a guy. I got a guy. Birdbones was born with hollow, super-fast bones giving him the ability to shit on anyone, instantly, for no reason, at any given time. Using the power of his kindergarten diploma and powerful Truck Nuts For Hats amulet he condescends even the most accomplished warriors. ',
+    image: birdBones,
+  },
+];
+
+export const aboutQuestions = [
+  {
+    q: 'WTF is TLDL?',
+    a: "MP3's to bullet points. We take longform audio and make it easily digestable and effortlessly shareable.",
+  },
+  {
+    q: 'What makes it different?',
+    a: 'We use our own custom AI model trained on crypto-specific terminology and jargon to produce accurate summaries and bullets on niche blockchain topics.',
+  },
+  {
+    q: 'What shows do you support?',
+    a: 'We currently only support The Daily Gwei Refuel as we iron out our proprietary model.',
+  },
+];
