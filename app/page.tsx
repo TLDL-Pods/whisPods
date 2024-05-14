bf7b329dca302afdef35b0126cffed9daa95accb
Christian <cjwinkler0@gmail.com>
2024-05-14 17:02:48 -0400

    fic homepage bg

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import Ticker from './components/HomeTicker';
 
 export default function Home() {
   return (
-    <div className="mx-auto h-screen w-full flex-col items-center justify-center">
-      <div className="flex w-full flex-col">
-        <div className="pt-24 lg:pt-0">
+    <div className="relative mx-auto h-screen w-full flex-col items-center justify-center">
+      <div className="flex w-full flex-col ">
+        <div className="z-10 pt-24 lg:pt-0">
           {/* TLDL Title */}
           <Image
-            className="z-10 mx-auto w-[250px] lg:mt-12 lg:w-[400px]"
+            className=" mx-auto w-[250px] lg:mt-12 lg:w-[400px]"
             src={'/tldl-logo-dm.svg'}
             alt={'TLDL'}
             width={500}
@@ -23,7 +23,7 @@ export default function Home() {
           <Ticker />
         </div>
       </div>
-      {/* <div className="absolute top-0 left-0 z-0 w-full h-full bg-black"></div> */}
+      <div className="absolute left-0 top-0 h-full w-full bg-black"></div>
     </div>
   );
 }
